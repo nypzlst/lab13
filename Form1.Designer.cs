98b5b381089229a295f4faae51d831970e9d8ed5
Oleksandr_Dvoreckyi <nyshtoprivet@gmail.com>
2024-06-09 21:02:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFullPath = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCreationTime = new System.Windows.Forms.Label();
+            this.lblLastAccessTime = new System.Windows.Forms.Label();
+            this.lblLastWriteTime = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(27, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(430, 400);
+            this.treeView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblLastWriteTime);
+            this.panel1.Controls.Add(this.lblLastAccessTime);
+            this.panel1.Controls.Add(this.lblCreationTime);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.lblFullPath);
+            this.panel1.Location = new System.Drawing.Point(535, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 214);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblFullPath
+            // 
+            this.lblFullPath.AutoSize = true;
+            this.lblFullPath.Location = new System.Drawing.Point(26, 24);
+            this.lblFullPath.Name = "lblFullPath";
+            this.lblFullPath.Size = new System.Drawing.Size(0, 13);
+            this.lblFullPath.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(26, 55);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 13);
+            this.lblName.TabIndex = 1;
+            // 
+            // lblCreationTime
+            // 
+            this.lblCreationTime.AutoSize = true;
+            this.lblCreationTime.Location = new System.Drawing.Point(26, 82);
+            this.lblCreationTime.Name = "lblCreationTime";
+            this.lblCreationTime.Size = new System.Drawing.Size(0, 13);
+            this.lblCreationTime.TabIndex = 2;
+            // 
+            // lblLastAccessTime
+            // 
+            this.lblLastAccessTime.AutoSize = true;
+            this.lblLastAccessTime.Location = new System.Drawing.Point(26, 110);
+            this.lblLastAccessTime.Name = "lblLastAccessTime";
+            this.lblLastAccessTime.Size = new System.Drawing.Size(0, 13);
+            this.lblLastAccessTime.TabIndex = 3;
+            // 
+            // lblLastWriteTime
+            // 
+            this.lblLastWriteTime.AutoSize = true;
+            this.lblLastWriteTime.Location = new System.Drawing.Point(26, 139);
+            this.lblLastWriteTime.Name = "lblLastWriteTime";
+            this.lblLastWriteTime.Size = new System.Drawing.Size(0, 13);
+            this.lblLastWriteTime.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFullPath;
+        private System.Windows.Forms.Label lblLastWriteTime;
+        private System.Windows.Forms.Label lblLastAccessTime;
+        private System.Windows.Forms.Label lblCreationTime;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+            treeView1.Dock = DockStyle.Fill;
+            Controls.Add(treeView1);
+            treeView1.BeforeSelect += treeView1_BeforeSelect;
+            treeView1.BeforeExpand += treeView1_BeforeExpand;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+            FillDriveNodes();
+         
+        }
+
+        void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node?.Nodes.Clear();
+            try
+            {
+                if (Directory.Exists(e.Node?.FullPath))
+                {
+                    string[] dirs = Directory.GetDirectories(e.Node.FullPath);
+                    foreach (string dir in dirs)
+                    {
+                        TreeNode dirNode = new TreeNode(new DirectoryInfo(dir).Name);
+                        FillTreeNode(dirNode, dir);
+                        e.Node.Nodes.Add(dirNode);
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+ 
+        void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node?.Nodes.Clear();
+            try
+            {
+                if (Directory.Exists(e.Node?.FullPath))
+                {
+                    string[] dirs = Directory.GetDirectories(e.Node.FullPath);
+
+                    foreach (string dir in dirs)
+                    {
+                        TreeNode dirNode = new TreeNode(new DirectoryInfo(dir).Name);
+                        FillTreeNode(dirNode, dir);
+                        e.Node.Nodes.Add(dirNode);
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        
+        private void FillDriveNodes()
+        {
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    TreeNode driveNode = new TreeNode { Text = drive.Name };
+                    FillTreeNode(driveNode, drive.Name);
+                    treeView1.Nodes.Add(driveNode);
+                }
+            }
+            catch (Exception) { }
+        }
+       
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // Get the selected node
+            TreeNode selectedNode = e.Node;
+
+            // Check if a node is selected
+            if (selectedNode != null)
+            {
+                // Display the properties of the selected node
+                string fullPath = selectedNode.FullPath;
+                string name = selectedNode.Text;
+                DirectoryInfo dirInfo = new DirectoryInfo(selectedNode.FullPath);
+                DateTime creationTime = dirInfo.CreationTime;
+                DateTime lastAccessTime = dirInfo.LastAccessTime;
+                DateTime lastWriteTime = dirInfo.LastWriteTime;
+
+                // Update the labels or controls with the node properties
+                lblFullPath.Text = fullPath;
+                lblName.Text = name;
+                lblCreationTime.Text = $"Creation time:"+creationTime.ToString();
+                lblLastAccessTime.Text = $"Last access time:" + lastAccessTime.ToString();
+                lblLastWriteTime.Text = $"Last write time:" + lastWriteTime.ToString();
+            }
+            else
+            {
+                // Clear the labels or controls if no node is selected
+                lblFullPath.Text = string.Empty;
+                lblName.Text = string.Empty;
+                lblCreationTime.Text = string.Empty;
+                lblLastAccessTime.Text = string.Empty;
+                lblLastWriteTime.Text = string.Empty;
+            }
+        }
+    }
+}
+
